85669259f4346e53cc065672cf63fe2e9e45f581
krab7191 <karstenrab91@gmail.com>
2014-09-02 18:29:21 -0400

    Unused older files.....

--- js/csv_reader.js
+++ js/csv_reader.js
@@ -6,8 +6,28 @@ $(document).ready(function () {
 	{
 		loadingText: "Loading data...",
 		loadingImage: "../img/loading_dots.gif"
-	}).bind("loadComplete",function() { 
+	}).bind("loadComplete",function() {
     	$('#CSVTable').find('TABLE').tablesorter();
+    	appendIDs();
+    	//appendLinks();
     });
+    
+    var appendIDs = function () {
+    	var $banking = $("td:contains('BANKING/ FINANCIAL')");
+    	console.log($banking.innerHTML);
+    	$banking.attr('id', 'banking');
+	};
+    
+    var appendLinks = function () {                                //FIX THIS IT'S BROKEN!!!!
+	    var $links = new Array;
+	    $links = $( "td:contains('http')" );
+        console.log($links);
+        for (var i=0; i<$links.length; i++){
+        	var text = $links[i].innerHTML;
+        	$links[i].empty;
+        	$links[i].append("<a>" + text + "</a>");
+        	console.log($links[i]);
+        };
+    };
 });
 
